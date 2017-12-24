52686961b70817a33b3c0898d0def2020dbce561
Sam Zipper <samuelczipper@gmail.com>
2017-12-24 09:44:01 -0600

    Add Tutorial 1 from FloPy GitHub

---
+++ GitHub-Tutorial1/GitHub-Tutorial1.py
@@ -0,0 +1,63 @@
+## Tutorial 1 from http://modflowpy.github.io/flopydoc/tutorial1.html
+# import flopy
+import flopy
+import numpy as np
+
+# where is your MODFLOW-2005 executable?
+path2mf = 'C:/Users/Sam/Dropbox/Work/Models/MODFLOW/MF2005.1_12/bin/mf2005.exe'
+
+# Assign name and create modflow model object
+modelname = 'tutorial1'
+mf = flopy.modflow.Modflow(modelname, exe_name=path2mf)
+
+# Model domain and grid definition
+Lx = 1000.
+Ly = 1000.
+ztop = 0.
+zbot = -50.
+nlay = 1
+nrow = 10
+ncol = 10
+delr = Lx/ncol
+delc = Ly/nrow
+delv = (ztop - zbot) / nlay
+botm = np.linspace(ztop, zbot, nlay + 1)
+
+# Create the discretization object
+dis = flopy.modflow.ModflowDis(mf, nlay, nrow, ncol, delr=delr, delc=delc,
+                               top=ztop, botm=botm[1:])
+
+# Variables for the BAS package
+ibound = np.ones((nlay, nrow, ncol), dtype=np.int32)
+ibound[:, :, 0] = -1
+ibound[:, :, -1] = -1
+strt = np.ones((nlay, nrow, ncol), dtype=np.float32)
+strt[:, :, 0] = 10.
+strt[:, :, -1] = 0.
+bas = flopy.modflow.ModflowBas(mf, ibound=ibound, strt=strt)
+
+# Add LPF package to the MODFLOW model
+lpf = flopy.modflow.ModflowLpf(mf, hk=10., vka=10.)
+
+# Add OC package to the MODFLOW model
+oc = flopy.modflow.ModflowOc(mf)
+
+# Add PCG package to the MODFLOW model
+pcg = flopy.modflow.ModflowPcg(mf)
+
+# Write the MODFLOW model input files
+mf.write_input()
+
+# Run the MODFLOW model
+success, buff = mf.run_model()
+
+# postprocess/plot results
+import matplotlib.pyplot as plt
+import flopy.utils.binaryfile as bf
+plt.subplot(1,1,1,aspect='equal')
+hds = bf.HeadFile(modelname+'.hds')
+head = hds.get_data(totim=1.0)
+levels = np.arange(1,10,1)
+extent = (delr/2., Lx - delr/2., Ly - delc/2., delc/2.)
+plt.contour(head[0, :, :], levels=levels, extent=extent)
+plt.show()
